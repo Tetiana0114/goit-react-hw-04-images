33b8a930b98ca899699fff84e178dde480a864fe
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-10-29 14:47:24 +0300

    Update ImageGalleryItem to hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,34 +1,27 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 import Modal from 'components/Modal';
 
-class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
-  }
-toggleModal = () => {
-  this.setState(({ showModal }) => ({
-  showModal: !showModal,
-  }));
-}
-render () {
-  const { showModal } = this.state;
-  const { webformatURL, tags } = this.props.item;
-  const { item } = this.props;
-  return (
-    <>
-     <li className={css.imageGalleryItem}>
-          <img className={css.imageGalleryItem_image}
-          onClick={this.toggleModal}
-          src={webformatURL}
-          alt={tags}
-          />
-    </li>
-    {showModal && (<Modal item={item} onClose={this.toggleModal}/>)}
-    </>
-  )}
-}
+const ImageGalleryItem = ({ item }) => {
+  const [showModal, setShowModal] = useState(false);
+  
+  const toggleModal = () => {
+  setShowModal(prev => !prev);
+};
+
+return (
+  <>
+   <li className={css.imageGalleryItem}>
+        <img className={css.imageGalleryItem_image}
+        onClick={toggleModal}
+        src={item.webformatURL}
+        alt={item.tags}
+        />
+  </li>
+  {showModal && (<Modal item={item} onClose={toggleModal}/>)}
+  </>
+)}
 
 ImageGalleryItem.propTypes = {
   item: PropTypes.shape({
