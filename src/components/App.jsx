79423ccc4dc3432bf230bf7952aa656cefaaeca0
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-10-30 22:25:59 +0200

    Update App to hooks/done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import fetchGallery from './Services/FetchGallery';
 import css from './App.module.css'
@@ -8,77 +8,65 @@ import Button from 'components/Button';
 import Loader from 'components/Loader';
 
 
-export class App extends Component {
-  state = {
-    error: null,
-    query: '',
-    items: [],
-    page: 0, 
-    loading: false,
-  }
+export const App = () => {
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-async componentDidUpdate(_, prevState) {
-    const prevQuery = prevState.query;
-    const prevPage = prevState.page;
-    const newQuery = this.state.query;
-    const newPage = this.state.page;
-    if (prevQuery !== newQuery || prevPage !== newPage) {
-    this.setState({ loading: true });
-    try {
-      const newItems = await fetchGallery({query: newQuery, page: newPage});
-    if (newItems.totalHits !== 0) {
-      this.setState({
-        items: [...this.state.items, ...newItems.hits],
-        })} 
-        else {
-          Notify.failure('Sorry, there are no images with this name.');
-        }
-        }
-    catch (error) {
-      this.setState({ error })
-    }
-    finally {
-      this.setState({ loading: false });
-      }
-    }
+  const showComponent = items.length > 0;
+
+const handleSearchFormSubmit = queryName => {
+    setPage(1);
+    setQuery(queryName);
+    setItems([]);
 }
-
-handleSearchFormSubmit = queryName => {
-  this.setState({
-    page: 1,
-    query: queryName,
-    items: [],
-    loading: true,
-  });
-}
-
-loadMore = () => {
-  this.setState(prevState => ({
-    page: prevState.page + 1,
-    loading: true,
-  }));
+  
+const loadMore = () => {
+    setPage(prev => prev + 1);
+    setLoading(true);
 };
 
+useEffect(() => {
+  if (query === '') {
+    return;
+}
+setLoading(true);
+const getNewQuery = async() => {
+  try {
+    const newItems = await fetchGallery({query: query, page: page});
+  if (newItems.totalHits !== 0) {
+    setItems(prev => [...prev, ...newItems.hits]);
+  } else {
+    Notify.failure('Sorry, there are no images with this name.');
+        }
+  }
+  catch (error) {
+      setError(error);
+  }
+  finally {
+      setLoading(false);
+  } 
+}
+getNewQuery();
+}, [page, query]);
 
-render () {
-const { items, loading, error } = this.state;
-const showComponent = items.length > 0;
 
 return (
-    <div
-    style={{
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: 40,
-      color: '#010101'
-    }}
-    >
-<SearchBar onSubmit={this.handleSearchFormSubmit}/>
+      <div
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+      >
+<SearchBar onSubmit={handleSearchFormSubmit}/>
 {error && <p className={css.text}>Whoops, something went wrong...</p>}
 {showComponent && (<ImageGallery items={items}/>)}
 {loading && (<Loader/>)}
-{showComponent && (<Button onClick={this.loadMore}/>)}
-    </div>
-  );
+{showComponent && (<Button onClick={loadMore}/>)}
+      </div>
+    );
 }
-};
